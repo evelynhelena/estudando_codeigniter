8c12cf895493181d3d631b40f3a28ee8f0a82e3e
Evelyn <evelynhelena781@gmail.com>
2021-05-03 19:47:09 -0300

    implementando deletar,consultar e alterar

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -26,6 +26,52 @@ class Usuario extends CI_Controller{
         echo json_encode($retorno);
     }
 
+    public function consultar(){
+        $usuario = $this->input->post('usuario');
+        $nome = $this->input->post('nome');
+        $tipo_usuario = strtoupper($this->input->post('tipo_usuario'));
+
+        if(trim($tipo_usuario) != 'ADMINISTRADOR' && trim($tipo_usuario) != 'COMUM' && trim($tipo_usuario) != ''){
+            $retorno = array('codigo' => 5, 'msg' => 'Tipo de usuário inválido');
+        }else{
+            $this->load->model('m_usuario');
+            $retorno = $this->m_usuario->consultar($usuario,$nome,$tipo_usuario);
+        }
+        echo json_encode($retorno);
+    }
+
+    public function alterar(){
+        $usuario = $this->input->post('usuario');
+        $senha = $this->input->post('senha');
+        $nome = $this->input->post('nome');
+        $tipo_usuario = $this->input->post('tipo_usuario');
+
+        if(trim($tipo_usuario) != 'ADMINISTRADOR' && trim($tipo_usuario) != 'COMUM' && trim($tipo_usuario) != ''){
+            $retorno = array('codigo' => 5, 'msg' => 'Tipo de usuário inválido');
+        }elseif(trim($usuario == '')){
+            $retorno = array('codigo' => 2, 'msg' => 'Usuário não informado');
+        }elseif(trim($senha == '')){
+            $retorno = array('codigo' => 4, 'msg' => 'Semha não informada');
+        }else{
+            $this->load->model('m_usuario');
+
+            $retorno = $this->m_usuario->alterar($usuario,$nome,$senha,$tipo_usuario);
+        }
+        echo json_encode($retorno);
+    }
+
+    public function desativar(){
+        $usuario = $this->input->post('usuario');
+        if(trim($usuario == '')){
+            $retorno = array('codigo' => 2, 'msg' => 'Usuário não informado');
+        }else{
+            $this->load->model('m_usuario');
+
+            $retorno = $this->m_usuario->desativar($usuario);
+        }
+        echo json_encode($retorno);
+    }
+
 }
 
 ?>
--- application/models/M_usuario.php
+++ application/models/M_usuario.php
@@ -13,6 +13,50 @@ class M_usuario extends CI_Model{
 
         return $dados;
     }
+
+    public function consultar($usuario,$nome,$tipo_usuario){
+        $sql = "select * from tbl_usuarios where dtcria >= '2020-08-01 00:00:00:'";
+        if($usuario != ''){
+            $sql = $sql . "and usuario = '$usuario'";
+        }
+        if($tipo_usuario != ''){
+            $sql = $sql . "and tipo = '$tipo_usuario'";
+        }
+        if($nome != ''){
+            $sql = $sql . "and nome like'%$nome%'";
+        }
+
+        $retorno = $this->db->query($sql);
+
+        if($retorno->num_rows() > 0){
+            $dados = array('codigo' => 1,'msg' => 'consulta efetuada com sucesso', 'data' => $retorno->result());
+        }else{
+            $dados = array('codigo' => 6,'msg' => 'Dados não encontrados');
+        }
+        return $dados;
+    }
+
+    public function alterar($usuario,$nome,$senha,$tipo_usuario){
+        $this->db->query("update tbl_usuarios set nome = '$nome', senha = '$senha', tipo ='$tipo_usuario' where usuario = '$usuario'");
+
+        if($this->db->affected_rows() > 0){
+            $dados = array('codigo' => 1, 'msg' => 'Usuário atualizado com sucesso');
+        }else{
+            $dados = array('codigo' => 6, 'msg' => 'Houve algum problema na atualização na tabela de usuários');
+        }
+
+        return $dados;
+    }
+
+    public function desativar($usuario){
+        $this->db->query("update tbl_usuarios set estatus = 'D' where usuario = '$usuario'");
+        if($this->db->affected_rows() > 0){
+            $dados = array('codigo' => 1,'msg' => 'Usuário Desativado com sucesso');
+        }else{
+            $dados = array('codigo' => 1,'msg' => 'Erro ao desativar o usuário');
+        }
+        return $dados;
+    }
 }
 
 ?>
